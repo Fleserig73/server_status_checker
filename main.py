ad9371db3f4777c495edc384ec8a4fcf715b755e
Fleserig73 <88104726+Fleserig73@users.noreply.github.com>
2023-11-23 22:09:20 +0100

    move from request to http

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import argparse
-import requests
+from http.client import HTTPSConnection
 
 def status_check(url):
-    response = requests.get(url)
-    return response.status_code
+    conn = HTTPSConnection(url)
+    conn.request("GET", "/")
+    response = conn.getresponse()
+    return response.status, response.reason
+
 
 parser = argparse.ArgumentParser(description='Server Status Checker')
+
 parser.add_argument('--url', type=str,
                     help='enter url')
 args = parser.parse_args()
+
 
 if args.url is None:
     url = input("Write url:   ")
 else:
     url = args.url
 
-print(status_check(url))
+print(" - ".join(map(str,status_check(url))))
